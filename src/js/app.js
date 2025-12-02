375e02f240eb572ccd06f7ac7670fc22e640a820
messend <aquilmessen@gmail.com>
2025-12-02 20:52:02 +0000

    redirige a cualquier pag web de usuario

--- src/js/app.js
+++ src/js/app.js
@@ -41,17 +41,17 @@ function render(variables = {}) {
     variables.country ? variables.country : "Country"
   }</h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/4geeksacademy${
-              variables.twitter ? variables.twitter : "Twitter"
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : ""
             }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy${
-              variables.github ? variables.github : "GitHub"
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : ""
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy${
-              variables.linkedin ? variables.linkedin : "linkedIn"
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin ? variables.linkedin : ""
             }"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy${
-              variables.instagram ? variables.instagram : "instagram"
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : ""
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
